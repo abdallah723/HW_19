6efe03f7967e14a4ad9ec288533954ea7bf4bae9
abdallah723 <ibrham11@gmail.com>
2020-06-12 18:36:43 -0400

    Updates

---
+++ precache-manifest.a8f41acbfac66d71629d9b7622944ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f243f5238d2775bbf73b6c9acdb005",
+    "url": "/HW_19/index.html"
+  },
+  {
+    "revision": "2917edd4b1e70e4a6cb8",
+    "url": "/HW_19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e666ab1c62e8517d6a2",
+    "url": "/HW_19/static/js/2.159e2d66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW_19/static/js/2.159e2d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2917edd4b1e70e4a6cb8",
+    "url": "/HW_19/static/js/main.1b3a5f28.chunk.js"
+  },
+  {
+    "revision": "efc1ec8870b3931edee7",
+    "url": "/HW_19/static/js/runtime-main.6c7b8466.js"
+  }
+]);
